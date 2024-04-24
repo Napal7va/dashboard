48fb7d13d4a134b4b404e8f119d20fe6ccc6179e
Mike <marko.v.polo.solo@gmail.com>
2024-04-24 19:42:30 +0300

    update for sanya

--- src/pages/master/dashboard.vue
+++ src/pages/master/dashboard.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="w-screen h-screen flex">
 <!--Side bar-->
-    <div class="w-[200px] h-full bg-gray-200">
-      Side Bar
+    <div class="w-[400px] h-full bg-gray-600">
+      <div class="h-[50px] bg-gray-800">
+        Header sidebar
+      </div>
+      <div class="h-[calc(100vh-50px)] bg-gray-200">
+        menu
+      </div>
     </div>
-    <div class="w-full h-full bg-gray-800">
-      main
-    </div>
+<!--Main-->
+  <div class="w-full h-full bg-white]">
+      <div class="w-full h-[50px] bg-gray-600">
+        main
+      </div>
+      <div class="h-[calc(100vh-50px)] bg-white]">
+
+      </div>
+      </div>
   </div>
 </template>
 
