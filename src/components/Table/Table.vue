986205504b18c6f12ce7dfb3cfb5514a7a09ae65
zhrnww <sasha.boy16@gmail.com>
2024-04-25 09:55:51 +0300

    parse students

---
+++ src/components/Table/Table.vue
@@ -0,0 +1,29 @@
+<template>
+  <table class="table-auto w-full">
+    <thead>
+    <tr>
+      <th v-for="column in columns" :key="column.key" class="px-4 py-2">{{ column.title }}</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="row in rows" :key="row.id">
+      <td v-for="column in columns" :key="column.key" class="border px-4 py-2">{{ row[column.key] }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    rows: {
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
--- src/pages/master/dashboard.vue
+++ src/pages/master/dashboard.vue
@@ -12,20 +12,40 @@
 <!--Main-->
   <div class="w-full h-full bg-white]">
       <div class="w-full h-[50px] bg-gray-600">
-        main
+        <span></span>
       </div>
       <div class="h-[calc(100vh-50px)] bg-white]">
-
+        <Table :rows="students" :columns="columns"/>
       </div>
       </div>
   </div>
 </template>
 
-<script>
-export default {
-}
+<script setup>
+import Table from "@/components/Table/Table.vue";
+import {computed, ref} from "vue";
+import {useFetch} from "@vueuse/core";
+
+const columns = ref( [
+  { title: "ID", key: "id", minWidth: true },
+  { title: "Имя ученика", key: "firstName" },
+  { title: "Фамилия", key: "lastName" }
+])
+
+const url = computed(() => {
+  return `https://my-json-server.typicode.com/kosipov/1425-iro-placeholder-api/disciplines`
+})
+
+const { isFetching, error, data } = useFetch(url, {
+  refetch: true
+}).json()
+
+const students = computed(() => {
+  if (data.value && data.value.length > 0) {
+    // Первый элемент массива data содержит информацию о студентах
+    return data.value[0].students;
+  } else {
+    return [];
+  }
+});
 </script>
-
-<style>
-
-</style>
